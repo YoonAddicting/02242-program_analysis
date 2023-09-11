6c4257dd06bc1c0a666cb8ca88356941230df3df
Jonas Pedersen <jonas@bpedersen.xyz>
2023-09-11 16:58:16 +0200

    Added example folder

---
+++ ass03/course-02242-examples/src/dependencies/java/dtu/deps/simple/OtherSuper.java
@@ -0,0 +1,12 @@
+package dtu.deps.simple;
+
+public class OtherSuper{
+    int a = 1;
+    private void what(){
+        for(int i = 0; i < a ;i++){
+            System.out.println("What?");
+        }
+        a++;
+        
+    }
+}
